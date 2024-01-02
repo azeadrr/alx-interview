415fd58a4ab3b06785b247487afc15578f461692
azeadrr <azeadn1@gmail.com>
2024-01-02 13:30:12 -0800

    0x00-pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,9 +7,9 @@ def pascal_triangle(n):
     """pascal triange
     """
     res = [[1]]
-    if num <= 0:
+    if n <= 0:
         return []
-    for i in range(num - 1):
+    for i in range(n - 1):
         temp = [0] + res[-1] + [0]
         row = []
         for x in range(len(res[-1]) + 1):
