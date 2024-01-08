fedfe3748c8e318697105ea3cc06c5364d2a2603
azeadrr <azeadn1@gmail.com>
2024-01-08 14:29:06 -0800

    0x01. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+method that determines if all boxes
+"""
+
+def canUnlockAll(boxes):
+    """
+    0. Lockboxes
+    """
+    if len(boxes) == 0:
+        return False
+    if len(boxes) == 1:
+        return True
+    keys = [0]
+    for key in keys:
+        for nkey in boxes[key]:
+            if nkey not in keys and nkey < len(boxes):
+                keys.append(nkey)
+    if len(keys) == len(boxes):
+        return True
+    return False
